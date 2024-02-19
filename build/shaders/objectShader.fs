3d8851476c8eecc45a86a1282ea6dbd9ebe1eaae
TestLaptop <testlaptop@laptop.com>
2024-02-19 09:02:04 +0100

    working camera spotlight

--- build/shaders/objectShader.fs
+++ build/shaders/objectShader.fs
@@ -41,10 +41,29 @@ struct PointLight {
 #define NR_POINT_LIGHTS 4
 uniform PointLight pointLights[NR_POINT_LIGHTS];
 
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+    float cosCutOff;
+    float cosOuterCutOff;
+
+
+    float constant;
+    float linear;
+    float quadratic;
+
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform SpotLight spotLight;
+
 uniform vec3 viewPos;
 
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 void main()
 {
@@ -56,6 +75,9 @@ void main()
     // Point lights
     for(int i = 0; i < NR_POINT_LIGHTS; i++)
         result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);
+
+    // Spot light
+    result += CalcSpotLight(spotLight, norm, FragPos, viewDir);    
 
     FragColor = vec4(result, 1.0);
 }
@@ -101,4 +123,26 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     specular *= attenuation;
 
     return (ambient + diffuse + specular);
+}
+
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+
+    // Diffuse
+    float diff = max(dot(normal, lightDir), 0.0);
+    // Specular
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // On combine les résultats
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+    // Intensity
+    float cosTheta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cosCutOff - light.cosOuterCutOff;
+    float intensity = clamp((cosTheta - light.cosOuterCutOff) / epsilon, 0.0, 1.0);
+    
+    return (ambient + diffuse + specular) * intensity;
 }
